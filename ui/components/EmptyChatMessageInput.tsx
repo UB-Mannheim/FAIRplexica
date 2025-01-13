1b183222f8e5bc6a2e3b5c0a11f38f5b2c65a32c
th-schmidt <thomas.schmidt@uni-mannheim.de>
2025-01-13 12:02:23 +0100

    Increase font size in empty chat input

--- ui/components/EmptyChatMessageInput.tsx
+++ ui/components/EmptyChatMessageInput.tsx
@@ -1,4 +1,4 @@
-import { ArrowRight } from 'lucide-react';
+import { ArrowRight, Search } from 'lucide-react';
 import { useEffect, useRef, useState } from 'react';
 import TextareaAutosize from 'react-textarea-autosize';
 import CopilotToggle from './MessageInputActions/Copilot';
@@ -84,12 +84,11 @@ const EmptyChatMessageInput = ({
           value={message}
           onChange={(e) => setMessage(e.target.value)}
           minRows={2}
-          className="bg-transparent placeholder:text-black/50 dark:placeholder:text-white/50 text-sm text-black dark:text-white resize-none focus:outline-none w-full max-h-24 lg:max-h-36 xl:max-h-48"
-          placeholder="Ask anything..."
+          className="bg-transparent placeholder:text-black/50 dark:placeholder:text-white/50 text-md text-black dark:text-white resize-none focus:outline-none w-full max-h-24 lg:max-h-36 xl:max-h-48"
+          placeholder="Ask a question ..."
         />
 
-        {/* ExpertTools */}
-        {/* Focus and attached Files */}
+        {/* Admin Tools: Focus, Attach Files */}
         <div className="flex flex-row items-center justify-between mt-4">
           {isAdminMode && (
               <div className="flex flex-row items-center space-x-2 lg:space-x-4">
@@ -107,8 +106,8 @@ const EmptyChatMessageInput = ({
               </div>
             )}
 
-          {/* Optimization Mode */}
-          <div className="flex flex-row items-center space-x-1 sm:space-x-4">
+          {/* Admin Tools: Optimization Mode */}
+          <div className={`flex flex-row items-center space-x-1 sm:space-x-4 ${!isAdminMode ? 'ml-auto' : ''}`}>
             {isAdminMode && (
                 <Optimization
                   optimizationMode={optimizationMode}
@@ -119,7 +118,7 @@ const EmptyChatMessageInput = ({
               disabled={message.trim().length === 0}
               className="bg-[#24A0ED] text-white disabled:text-black/50 dark:disabled:text-white/50 disabled:bg-[#e0e0dc] dark:disabled:bg-[#ececec21] hover:bg-opacity-85 transition duration-100 rounded-full p-2"
               >
-              <ArrowRight className="bg-background" size={17} />
+              <Search className="bg-background" size={17} />
             </button>
           </div>
         </div>

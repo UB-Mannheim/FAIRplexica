3cc73dd07aac40fecf131874aa2fc07be0fd4a24
th-schmidt <thomas.schmidt@uni-mannheim.de>
2025-03-05 13:08:32 +0100

    Update about page

--- ui/app/about/page.tsx
+++ ui/app/about/page.tsx
@@ -1,35 +1,11 @@
 'use client';
 
-import { Info } from 'lucide-react';
-
 const AboutPage = () => {
   return (
-    <div>
-      <div className="flex flex-col pt-4">
-        <div className="flex items-center">
-          <Info />
-          <h1 className="text-3xl font-medium p-2">About FAIRplexica</h1>
-        </div>
-        <hr className="border-t border-[#2B2C2C] my-4 w-full" />
-      </div>
+      <main className="flex flex-col items-center justify-center min-h-screen max-w-screen-lg mx-auto p-2">
 
-      {/* Main Content */}
-      <main className="px-4 lg:px-8 py-4 lg:py-8">
-
-        <div className="flex justify-center gap-12 mb-10 h-24">
-          <img
-            src="/UB_logo.svg"
-            alt="Mannheim University Library Logo"
-          />
-          <img
-            src="/FDZ_logo.png"
-            alt="Research Data Center Mannheim Logo"
-          />
-        </div>
-
-        <p className="text-black/70 dark:text-white/70 text-lg md:text-lg">
-          FAIRplexica is an open source academic search engine designed to answer questions about{' '}
-          <b>research data management (RDM)</b>.
+        <p className="text-black/70 dark:text-white/70 text-lg md:text-lg text-center">
+          <b>FAIRplexica</b> is an open source academic search engine designed to answer questions about research data management (RDM).
           It performs guided web searches over relevant RDM resources using the metasearch engine{' '}
           <a
             href="https://github.com/searxng/searxng"
@@ -38,7 +14,7 @@ const AboutPage = () => {
             className="text-black dark:text-white lg:text-lg font-medium truncate transition duration-200 hover:text-[#ADB8C8] dark:hover:text-[#ADB8C8] cursor-pointer"
           >
             SearXNG
-          </a>. The collected search results are then processed by a local LLM using{' '}
+          </a>. The collected search result are then processed by a local LLM using{' '}
           <a
             href="https://github.com/ollama/ollama"
             target="_blank"
@@ -46,8 +22,8 @@ const AboutPage = () => {
             className="text-black dark:text-white lg:text-lg font-medium truncate transition duration-200 hover:text-[#ADB8C8] dark:hover:text-[#ADB8C8] cursor-pointer"
           >
             Ollama
-          </a>.<br /><br />
-          FAIRplexica is provided and maintained by{' '}
+          </a>.<br/><br/>
+          FAIRplexica is maintained by{' '}
           <a
             href="https://www.bib.uni-mannheim.de/en/"
             target="_blank"
@@ -66,8 +42,34 @@ const AboutPage = () => {
             Perplexica
           </a>.
         </p>
+
+        <div className="flex items-center gap-8 mt-8">
+          <a
+            href="https://www.bib.uni-mannheim.de/en/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="group"
+           >
+            <img
+              src="/UB_logo.svg"
+              alt="Mannheim University Library Logo"
+              className="h-20 group-hover:filter hover:grayscale transition duration-300 ease-in-out cursor-pointer"
+            />
+          </a>
+          <a
+            href="https://www.bib.uni-mannheim.de/lehren-und-forschen/forschungsdatenzentrum/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="group"
+           >
+            <img
+              src="/FDZ_logo.png"
+              alt="Research Data Center Mannheim Logo"
+              className="h-20 group-hover:filter hover:grayscale transition duration-300 ease-in-out cursor-pointer"
+            />
+          </a>
+        </div>
       </main>
-    </div>
   );
 };
 
